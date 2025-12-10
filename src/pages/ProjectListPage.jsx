e93e039083bfa450a6d8dbdcdfe3a46f5564eb20
Jorge Jiménez <gexgt90@gmail.com>
2025-12-10 10:56:18 +0100

    implements list of projects

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,9 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 
 function ProjectListPage() {
+
+    const [projects, setProjects] = useState(null)
+
+    useEffect(() => {
+        axios.get("https://project-management-api.edu.ironhack.com/projects/")
+        .then((response) => {
+            setProjects(response.data)
+        })
+        .catch((error) => {
+            console.log("error", error)
+        })
+    }, [])
+
+    if(projects === null) {
+        return <Loader />
+    }
 
     return (
         <div>
             <h1>Project List Page</h1>
+            <h2>Number of Projects: {projects.length}</h2>
+
+            {projects.map((elm) => {
+                return (
+                    <div className="card" key={elm.id}>
+                        <h3>{elm.title}</h3>
+                        <p>{elm.description}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
